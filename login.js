2148aa2ed936e27f20dee4abadc8efb02a72e8ee
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-01-14 00:22:55 +0530

    added login and register code

---
+++ login.js
@@ -0,0 +1,28 @@
+// validating the user
+const loginButton = document.getElementById("login-btn");
+
+loginButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    var email = document.getElementById('login-email').value;
+    var password = document.getElementById('login-pass').value;
+    console.log(password, "user pass");
+    validateUser(email, password);
+
+})
+
+async function validateUser(email, password) {
+    const data = await axios.get(`http://localhost:3000/users?email=${email}`);
+    console.log(data);
+    if (data.data.length === 0) {
+        alert("user not found");
+    } else {
+        if (data.data[0].password === password) {
+            sessionStorage.setItem("name", data.data[0].name)
+            sessionStorage.setItem("id", data.data[0].id)
+            window.location.href = "file:///home/mahesh/housemonk/javascript/todo_app/index.html";
+        } else {
+            console.log("password is wrong");
+        }
+    }
+}
---
+++ register.js
@@ -0,0 +1,24 @@
+// adding new user
+
+const button = document.getElementById("register-btn");
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    var name = document.getElementById("reg-name").value;
+    var email = document.getElementById("reg-email").value;
+    var password = document.getElementById("reg-pass").value;
+    console.log(name);
+    if (name === "" || email === "" || password === "") {
+        alert("somethibng is epyty");
+    }
+    addUser(name, email, password);
+})
+
+async function addUser(name, email, password) {
+    const data = await axios.post("http://localhost:3000/users", {
+        name, email, password
+    });
+    sessionStorage.setItem("name", name)
+    sessionStorage.setItem("id", data.data.id)
+    window.location.href = "file:///home/mahesh/housemonk/javascript/todo_app/index.html";
+}
